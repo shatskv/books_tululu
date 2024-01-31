e8519e9b6572521c51637a44332e0725af23336d
Vladimir Shatskiy <vovaysach@gmail.com>
2024-02-01 02:31:08 +0300

    fixes for deploy

--- books_library/settings.py
+++ books_library/settings.py
@@ -138,11 +138,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "/static/"
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, 'static'),
-# ]
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 # STATIC_ROOT = ''
+CSRF_TRUSTED_ORIGINS = os.getenv('CSRF_TRUSTED_ORIGINS').split(',')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
